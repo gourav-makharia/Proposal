6710b3aad1625b9cb3a7b426ae16dab82350b742
gourav-makharia <gourav.makharia@gmail.com>
2026-07-25 12:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,91 @@
-const startBtn=document.getElementById("startBtn");
+const startBtn = document.getElementById("startBtn");
+const welcome = document.getElementById("welcomeMessage");
 
-const welcome=document.getElementById("welcomeMessage");
+const continueBtn = document.getElementById("continueBtn");
 
-startBtn.onclick=function(){
+const storyPage = document.getElementById("storyPage");
+const storyText = document.getElementById("storyText");
+const nextBtn = document.getElementById("nextBtn");
 
-startBtn.style.display="none";
+const hearts = document.getElementById("hearts");
 
-welcome.classList.remove("hidden");
+// Floating hearts
+setInterval(() => {
 
-}
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+
+    heart.style.fontSize = (20 + Math.random() * 20) + "px";
+
+    hearts.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+
+}, 350);
+
+// Start button
+startBtn.onclick = function () {
+
+    startBtn.style.display = "none";
+
+    welcome.classList.remove("hidden");
+
+};
+
+// Continue button
+continueBtn.onclick = function () {
+
+    welcome.classList.add("hidden");
+
+    storyPage.classList.remove("hidden");
+
+    const message =
+        "There is something I've been wanting to tell you...\n\nI wasn't sure how to say it...\n\nSo I thought I'd build a little website instead. 😊";
+
+    let i = 0;
+
+    storyText.innerHTML = "";
+
+    function typeWriter() {
+
+        if (i < message.length) {
+
+            if (message.charAt(i) === "\n") {
+
+                storyText.innerHTML += "<br>";
+
+            } else {
+
+                storyText.innerHTML += message.charAt(i);
+
+            }
+
+            i++;
+
+            setTimeout(typeWriter, 45);
+
+        } else {
+
+            nextBtn.classList.remove("hidden");
+
+        }
+
+    }
+
+    typeWriter();
+
+};
+
+// Next button
+nextBtn.onclick = function () {
+
+    alert("🎉 Great! In the next lesson, we'll build the fun questions screen.");
+
+};
