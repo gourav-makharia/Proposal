081d2a86137d9a482e12ce70c3433821b06e01c5
gourav-makharia <gourav.makharia@gmail.com>
2026-07-25 11:38:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-document.getElementById("startBtn").addEventListener("click", function(){
+const hearts=document.getElementById("hearts");
 
-    alert("Welcome 😊\n\nThis is just the beginning of our story... ❤️");
+setInterval(()=>{
 
-});
+const heart=document.createElement("div");
+
+heart.className="heart";
+
+heart.innerHTML="❤️";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.fontSize=(20+Math.random()*20)+"px";
+
+hearts.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+},350);
+
+document.getElementById("startBtn").onclick=function(){
+
+alert("Welcome ❤️\n\nOur story is about to begin...");
+
+};
